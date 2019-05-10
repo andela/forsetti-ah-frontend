f921acafb22afa2b006f1703189d0e9c4c0d651c
Christopher Iliyas <christopher.iliyas@andela.com>
2019-05-10 15:24:00 +0100

    165937607-task(frontend): fix color on 404 page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,13 +1,13 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import LandingPage from '../containers/LandingPage';
+import LandingPageComponent from '../containers/LandingPage';
 import NotFound from '../components/NotFound';
 
 const Routes = () => (
   <Fragment>
     <Router>
       <Switch>
-        <Route path='/' component={LandingPage} exact />
+        <Route path='/' component={LandingPageComponent} exact />
         <Route component={NotFound} />
       </Switch>
     </Router>
